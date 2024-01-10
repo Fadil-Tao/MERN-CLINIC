3afe7090b740258913feec78f20ff562668c13a0
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-01-10 23:27:00 +0800

    updating layouts

--- Frontend/src/Layouts/AuthLayout.tsx
+++ Frontend/src/Layouts/AuthLayout.tsx
@@ -1,7 +1,20 @@
-const AuthLayout = () => {
-    return <div>
-        Hello world
-    </div>
+import AuthBanner from '../assets/AuthBanner.jpg';
+
+type AuthLayoutType = {
+    Form:React.ComponentType;
 }
+
+const AuthLayout = ({Form}:AuthLayoutType) => {
+    return (
+        <div className='min-h-screen flex justify-center items-center flex-wrap'>
+            <div>
+                <div>
+                    <img src={AuthBanner} alt='Image' />
+                </div>
+            </div>
+            <Form/>
+        </div>
+    );
+};
 
 export default AuthLayout;
--- Frontend/src/Layouts/RootLayout.tsx
+++ Frontend/src/Layouts/RootLayout.tsx
@@ -1,7 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import NavBar from '../Components/NavBar';
+import Footer from '../Components/Footer';
+
 const RootLayout = () => {
-    return <>
-        Hello rootlayout
-    </>
-}
+    return (
+        <>
+            <NavBar />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
 
 export default RootLayout;
