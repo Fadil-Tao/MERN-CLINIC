b9c19a55378f001aa55f1e6b9e6ddd4639120600
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-01-13 23:45:36 +0800

    updating the routes

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter } from 'react-router-dom'
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import RootLayout from './Layouts/RootLayout';
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
+import HomePage from './Pages/HomePage';
+import DoctorsPage from './Pages/DoctorsPage';
+import ProfilePage from './Pages/ProfilePage';
+import Appointment from './Pages/AppointmentPage';
 
 
 const router = createBrowserRouter([
-
-
-  
+      {
+        path:"/",
+        element:<RootLayout/>,
+        children:[
+          {
+            path:"home",
+            element: <HomePage/>,
+          },
+          {
+            path:"appointment",
+            element: <Appointment/>,
+          },
+          {
+            path:"doctors",
+            element: <DoctorsPage/>,
+          },
+          {
+            path:"profile",
+            element: <ProfilePage/>
+          }
+          
+        ]
+      },
+      {
+        path:"/login",
+        element: <LoginPage/>,
+      },
+      {
+        path:"/register",
+        element: <RegisterPage/>,
+      },
 ]); 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
+     <RouterProvider router={router}/>
   </React.StrictMode>,
 )
