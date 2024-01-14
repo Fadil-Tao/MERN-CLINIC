42ee6333d36d19e7334c318c6fdd6786437d7923
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-01-14 10:09:28 +0800

    add buttons component

---
+++ Frontend/src/Components/Buttons/CancelButton.tsx
@@ -0,0 +1,7 @@
+export const CancelButton = () => {
+    return (
+        <button className='bg-red-500 rounded-lg text-white font-medium p-1'>
+            Cancel
+        </button>
+    );
+};
--- Frontend/src/Components/Buttons/NavAuthButton.tsx
+++ Frontend/src/Components/Buttons/NavAuthButton.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const NavAuthButtons = () => {
     return (
-        <div className='flex'>
+        <div className='md:flex '>
             <div className='flex '>
                 <div className='mx-1'>
                     <Link to={'login'}>Login</Link>
@@ -13,6 +13,9 @@ const NavAuthButtons = () => {
             </div>
             <div className='hidden'>
                 <Link to={'profile'}>Profile</Link>
+            </div>
+            <div className='hidden'>
+                <Link to={'appointment'}>My Appointment</Link>
             </div>
         </div>
     );
