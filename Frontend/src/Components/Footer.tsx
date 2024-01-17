a123f030191300afe2332ff1581476d57b47c450
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-01-18 01:31:28 +0800

    updating the footer

--- Frontend/src/Components/Footer.tsx
+++ Frontend/src/Components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className=' bg-[#035628] text-white py-8 px-20 font-semibold'>
 
             <div className='text-center mt-4'>
-                <p>&copy; 2023 Your Medical Website. All rights reserved.</p>
+                <p>&copy; 2023 Medical Website. All rights reserved.</p>
             </div>
         </footer>
     );
