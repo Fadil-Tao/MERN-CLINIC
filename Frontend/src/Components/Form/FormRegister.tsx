943014b83e44763bfefe9d8172412a0e13b3987a
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-01-18 01:26:59 +0800

    add the method and fucntionality

--- Frontend/src/Components/Form/FormRegister.tsx
+++ Frontend/src/Components/Form/FormRegister.tsx
@@ -1,52 +1,88 @@
 import AuthInput from '../Inputs/AuthInput';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../Hooks/FormUtils.hook';
+import Register from '../../Services/Register';
+import { useRedirecting } from '../../Hooks/Redirecting.hook';
+import { useSnackbar } from "notistack";
 
 const FormRegister = () => {
+    const redirect = useRedirecting('/login');
+    const {enqueueSnackbar} = useSnackbar()
+    const { formData, handleChange, handleSubmit } = useForm({
+        _name: '',
+        _email: '',
+        _phone: 0,
+        _birth: '',
+        _password: '',
+    });
+    const registering = async () => {
+        await Register(formData)
+            .then(() => {
+                enqueueSnackbar('Succesfully Registering' , {
+                    variant:"success", autoHideDuration:3000
+                })
+                redirect();
+            })
+            .catch((err) => {
+                enqueueSnackbar(`${err}`, {
+                    variant:"error", autoHideDuration:3000
+                })
+                console.log(err)});
+    };
     return (
         <div className='border-2 shadow-xl border-green-700 p-12 rounded-md '>
             <div className='grid grid-cols-1 '>
                 <h1 className=' text-green-700 text-4xl font-bold'>Register</h1>
             </div>
-            <form action=''>
+            <form
+                onSubmit={(e) => {
+                    handleSubmit(e);
+                    registering();
+                }}
+            >
                 <AuthInput
                     type='text'
                     placeholder='Your Name'
-                    name='name'
+                    name='_name'
                     title='Name'
+                    handleChange={handleChange}
                 />
                 <AuthInput
                     type='email'
                     placeholder='Your Email'
-                    name='email'
+                    name='_email'
                     title='Email'
+                    handleChange={handleChange}
                 />
                 <AuthInput
                     type='tel'
                     placeholder='Your Phone Number'
-                    name='number'
+                    name='_phone'
                     title='Number'
+                    handleChange={handleChange}
                 />
                 <AuthInput
                     type='date'
                     placeholder='Your Birth Date'
-                    name='birth'
+                    name='_birth'
                     title='Birth'
+                    handleChange={handleChange}
                 />
                 <AuthInput
                     type='password'
                     placeholder='Your Password'
-                    name='password'
+                    name='_password'
                     title='Password'
+                    handleChange={handleChange}
                 />
-                <AuthInput
-                    type='password'
-                    placeholder='Confirm Password'
-                    name='password'
-                    title='Recheck Password'
-                />
-                <button className='w-full bg-green-700 rounded-full  py-1.5 text-white'>
-                    Register
-                </button>
+                <div>
+                    <button
+                        className='w-full bg-green-700 rounded-full  py-1.5 text-white'
+                        type='submit'
+                    >
+                        Register
+                    </button>
+                </div>
             </form>
             <p className=' text-sm mt-5 text-center'>
                 Have An Account?{' '}
