c7cde21e06c850915b27282912a930f1a212eb10
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-01-18 01:26:45 +0800

    add the method and fucntionality

--- Frontend/src/Components/Form/FormLogin.tsx
+++ Frontend/src/Components/Form/FormLogin.tsx
@@ -1,26 +1,53 @@
 import AuthInput from '../Inputs/AuthInput';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../Hooks/FormUtils.hook';
+import { HandleLogin } from '../../Services/Login';
+import { useRedirecting } from '../../Hooks/Redirecting.hook';
+import { AuthFunc } from '../../Store/auth';
 
 const FormLogin = () => {
+    const { statusLog } = AuthFunc();
+
+    const redirect = useRedirecting('/home');
+    const { formData, handleChange, handleSubmit } = useForm({
+        _email: '',
+        _password: '',
+    });
+    
+    const loginProc = async () => {
+        (await HandleLogin(formData)) && statusLog(true);
+        redirect();
+    };
+    
     return (
         <div className='border-2 shadow-xl border-green-700 p-12 rounded-md '>
             <div className='grid grid-cols-1 '>
                 <h1 className=' text-green-700 text-4xl font-bold'>Login</h1>
             </div>
-            <form action=''>
+            <form
+                onSubmit={(e) => {
+                    handleSubmit(e);
+                    loginProc();
+                }}
+            >
                 <AuthInput
                     type='email'
                     placeholder='Your Email'
-                    name='email'
+                    name='_email'
                     title='Email'
+                    handleChange={handleChange}
                 />
                 <AuthInput
                     type='password'
                     placeholder='Your Password'
-                    name='password'
+                    name='_password'
                     title='Password'
+                    handleChange={handleChange}
                 />
-                <button className='w-full bg-green-700 rounded-full  py-1.5 text-white'>
+                <button
+                    className='w-full bg-green-700 rounded-full  py-1.5 text-white'
+                    type='submit'
+                >
                     Login
                 </button>
             </form>
