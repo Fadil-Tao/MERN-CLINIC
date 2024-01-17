bf4b8a1eb7f1c889e39ca3a221bc9894b7e6c9bd
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-01-18 01:25:13 +0800

    add functionality button to send delete req

--- Frontend/src/Components/Buttons/CancelButton.tsx
+++ Frontend/src/Components/Buttons/CancelButton.tsx
@@ -1,6 +1,35 @@
-export const CancelButton = () => {
+import { useSnackbar } from 'notistack';
+import { deleteAppointmentData } from '../../Services/Appointment';
+type cancelPropsType = {
+    _id: string;
+};
+
+export const CancelButton = ({ _id }: cancelPropsType) => {
+    const { enqueueSnackbar } = useSnackbar();
+
+    const handleCancel = () => {
+        deleteAppointmentData(_id)
+            .then((response) => {
+                console.log(response);
+                enqueueSnackbar('Succesfully Deleting data', {
+                    variant: 'success',
+                    autoHideDuration: 3000,
+                });
+                window.location.reload();
+            })
+            .catch((err) => {
+                console.log(err);
+                enqueueSnackbar('Error on deleting data', {
+                    variant: 'error',
+                    autoHideDuration: 3000,
+                });
+            });
+    };
     return (
-        <button className='bg-red-500 rounded-lg text-white font-medium p-1'>
+        <button
+            className='bg-red-500 rounded-lg text-white font-medium p-1'
+            onClick={() => handleCancel()}
+        >
             Cancel
         </button>
     );
