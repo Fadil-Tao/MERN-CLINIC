b482495294e57518561bacf8d56515a4fc1a0c47
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-01-18 02:37:47 +0800

    removing try catch block

--- Frontend/src/Services/Login.ts
+++ Frontend/src/Services/Login.ts
@@ -1,30 +1,25 @@
-
 type userDataType = {
     _email: string;
     _password: string;
 };
 
 export const HandleLogin = async (userData: userDataType) => {
-    try {
-        const response = await fetch('http://localhost:8000/api/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-        });
+    const response = await fetch('http://localhost:8000/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
 
-        if (response.ok) {
-            const {
-                data: { token, _id},
-            } = await response.json();
-            localStorage.setItem('_id', _id);
-            localStorage.setItem('token', token);
-            return true;
-        } else {
-            throw new Error('Password or Email is wrong');
-        }
-    } catch (error) {
-        console.log(error)
+    if (response.ok) {
+        const {
+            data: { token, _id },
+        } = await response.json();
+        localStorage.setItem('_id', _id);
+        localStorage.setItem('token', token);
+        return true;
+    } else {
+        throw new Error('Email or Password might me wrong , also check your network');
     }
 };
